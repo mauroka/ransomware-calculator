8c0a6a67c9a2c472e6be1dc76761c3a37a04e267
DanteRodriguez2002 <danterodriguez2002@gmail.com>
2019-11-13 15:08:53 -0300

    Incluimos bootstrap y ya esta la parte de datos generales y backups

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,14 @@
 
 <script>
 import Calculator from './components/Calculator.vue'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+Vue.use(BootstrapVue)
 export default {
   name: 'app',
   components: {
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,16 +1,64 @@
 <template>
+
   <div >
-    <h3>asdasd</h3>
-    <form>
-    <label for="hLab">Cantidad de horas laborales</label>
-    <input type="number" name="hLab">
-    <label for="cHoraEmpleado">Costo de la hora por empleado</label>
-    <input type="number" name="cHoraEmpleado">  
-    </form>    
+    
+    
+    <h1 class="display-4">Ransomware</h1>
+
+      
+    <h2 class="display-5">Datos Generales</h2>
+
+
+    <form class="container" id="datosGenerales">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="hLab">Cantidad de horas laborales</label>
+          <input type="number" class="form-control" id="hLab" placeholder="">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="cHoraEmpleado">Costo de la hora por empleado</label>
+          <input type="number" class="form-control" id="cHoraEmpleado" placeholder="">
+          </div>
+      </div>
+      
+    
+
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+
+    <h2 class="display-5 mt-5">Backups</h2>
+
+    <form class="container" id="backups">
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="porcenBackup">Porcentaje de informacion respaldada</label>
+          <input type="number" class="form-control" id="porcenBackup" placeholder="">
+        </div>
+        <div class="form-group col-md-6">
+          <label for="chRegeInfo">Horas p/usuario para generar informacion no respaldada</label>
+          <input type="number" class="form-control" id="chRegeInfo" placeholder="">
+          </div>
+      </div>
+      <div class="form-group">
+        <label for="chUltiBackup">Cantidad de horas laborables desde el ultimo backup</label>
+        <input type="number" class="form-control" id="chUltiBackup" placeholder="">
+
+       
+
+      </div>
+      <div class="form-group ">
+         <label for="chLabSinBackup">Cantidad de horas laborales por usuario/equipo para recuperar informacion de la organizacion sin respaldar</label>
+        <input type="number" class="form-control" id="chLabSinBackup" placeholder="">
+      </div>
+    
+
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
   </div>
 </template>
 
 <script>
+
 export default {
   
 }
@@ -18,5 +66,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#datosGenerales{
+  padding: 2%;
+  border-style: solid;
+}
 
+#backups{
+  padding: 2%;
+  border-style: solid;
+}
 </style>
