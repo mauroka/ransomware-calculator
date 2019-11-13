cbf5ead13055b2f6a539994477c1084e1b12bcd7
DanteRodriguez2002 <danterodriguez2002@gmail.com>
2019-11-13 13:55:30 -0300

    config

---
+++ src/components/Calculator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div >
+    <h3>asdasd</h3>
+    <form>
+    <label for="hLab">Cantidad de horas laborales</label>
+    <input type="number" name="hLab">
+    <label for="cHoraEmpleado">Costo de la hora por empleado</label>
+    <input type="number" name="cHoraEmpleado">  
+    </form>    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
