aa976134d18980fcbe100109a0c23216d6009bd6
Pablo Abratte <pabratte@gmail.com>
2020-05-07 14:46:19 -0300

    Chart Fix

--- js/script.js
+++ js/script.js
@@ -193,6 +193,45 @@ const app = new Vue({
   
 
         },
+        showChart(){
+            var ctx = document.getElementById('myBarChart').getContext('2d');
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        },
         isValid: function(v){
             if(v === undefined || isNaN(v) || v==="" || v<"1"){
                 
@@ -242,6 +281,7 @@ const app = new Vue({
                     if (this.isValid(this.porcenEquiposInfectados) && this.isValid(this.cOportunidadVentas)) {
                         this.error = false
                         this.mostrar()
+                        this.showChart()
                     } else {
                         this.error = true
                     }
