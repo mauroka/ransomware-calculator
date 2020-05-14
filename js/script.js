ef38842fe0489178a10845e5630cfd3607d1bf1a
FabriQuinteros <fabriquinterosproa@gmail.com>
2020-05-14 20:31:09 -0300

    validation

--- js/script.js
+++ js/script.js
@@ -295,7 +295,7 @@ const app = new Vue({
 
         isValid: function(v){
             if(v === undefined || isNaN(v) || v==="" || v<"1"){
-                return true
+                return false;
             }else{
                 return true;
             }
