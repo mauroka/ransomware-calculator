0bc517cb963ff184d6edc40a1e3e60db46b08fb4
Pablo Abratte <pabratte@gmail.com>
2020-09-17 17:35:22 -0300

    scenario-card component

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+
+// components
+Vue.component('scenario-card', {
+    computed: {
+        textClass: function(){
+            return 'text-'+this.colorClass;
+        },
+        borderClass: function(){
+            return 'border-left-'+this.colorClass;
+        }
+    },
+    props: ['label', 'value', 'color-class'],
+    template: `
+    <div class="col-lg-4 col-md-6 mb-4 btn-escenario" v-on:click="$emit('clicked');">
+        <div class="card shadow h-100 py-2" :class="borderClass">
+            <div class="card-body">
+                <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <div class="text-xs font-weight-bold text-uppercase mb-1" :class="textClass">{{ label }}</div>
+                        <div class="h5 mb-0 font-weight-bold text-gray-800">$ {{value}} </div>
+                    </div>
+                    <div class="col-auto">
+                        <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+    });
+
+
 const app = new Vue({
     el:'#app',
     data:{
@@ -573,3 +605,4 @@ const app = new Vue({
         }
     }
 })
+
