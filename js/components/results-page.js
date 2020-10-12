24587cfda7b056e6cd0e4dbd66d2d1526af7cde4
FabriQuinteros <fabriquinterosproa@gmail.com>
2020-10-12 18:38:51 -0300

    pdf change

--- js/components/results-page.js
+++ js/components/results-page.js
@@ -63,21 +63,20 @@ Vue.component('results-page', {
                         console.error('Ha ocurrido un error', JSON.stringify(error));
                     });
             }
-
-
             await renderRef(doc, this.$refs["global-chart"])
+            await renderRef(doc, this.$refs["inputs"])
+            
 
+            
             for(var i=0; i<this.data_scenarios.length; i++){
                 var scenario_name = this.data_scenarios[i].nombre
                 var elem = this.$refs[scenario_name][0].$vnode.elm
                 this.showOnlyDetails = false
                 await renderRef(doc, elem)
-                this.showOnlyDetails = true
-                await renderRef(doc, elem)
             }
 
             // UNDO report mode
-            this.reportViewode = false
+            this.reportView = false
             wrapper.style.removeProperty("width")
             wrapper.style.fontSize="1em";
             doc.save("a4.pdf");
@@ -273,6 +272,7 @@ Vue.component('results-page', {
             dict_tacanio['key'] =  dict_tacanio['nombre'] +"_"+  Date.now()
             this.update_scenario("Tacaño", dict_tacanio);
             //console.log(JSON.stringify(this.data_scenarios))
+            
             // otro
             /*var otro = {}
             Object.assign(otro, this.userData);
@@ -294,7 +294,7 @@ Vue.component('results-page', {
         },
     },
     template: `
-    <div class="col-lg-12">                        
+    <div class="col-lg-12">              
         <div class="row"  ref="global-chart">
             <div class="col-12">
                 <div class="card shadow mb-4">
@@ -312,12 +312,15 @@ Vue.component('results-page', {
                     </div>
                 </div>
             </div>
+            
                                     
             <div class="col-12">
                 <div v-show="!reportView" class="alert alert-warning" role="alert">
                     Haga clic en los siguientes indicadores para ver su detalle.
                 </div>
             </div>
+
+            
         
             <!--ESCENARIOS-->
             <scenario-card v-for="scenario in data_scenarios"
@@ -329,6 +332,128 @@ Vue.component('results-page', {
                 >
             </scenario-card>
         </div>
+        <div class="col-12" ref="inputs">
+            <div v-show="reportView" class="alert alert-warning" role="alert">
+                <table class="table table-striped table-bordered table-hover specific-scenario-details report-page">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Campos</th>
+                        <th scope="col">Datos ingresados</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Duración en horas de la jornada laboral en su organización</th>
+                        <td scope="col">{{userData.hLab}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Costo promedio por hora de un empleado administrativo de su organización</th>
+                        <td scope="col">{{userData.cHorasEmpleado}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Cantidad de equipos informáticos de su organización (PC, ordenadores, desktops)</th>
+                        <td scope="col">{{userData.cantEquipos}}</td>
+                    </tr>
+                </tbody>
+                </table>
+
+                <table class="table table-striped table-bordered table-hover specific-scenario-details report-page">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Campos</th>
+                        <th scope="col">Datos ingresados</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Frecuencia en horas mediante la cual se realizan copias de seguridad en su organización</th>
+                        <td scope="col">{{userData.chUltiBackup}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">. ¿Cuántas horas laborales piensa que necesitaría trabajar cada usuario para regenerar toda la información que se considere relevante en su puesto de trabajo?</th>
+                        <td scope="col">{{userData.chRegeInfoTotal}}</td>
+                    </tr>
+                </tbody>
+                </table>
+
+                <table class="table table-striped table-bordered table-hover specific-scenario-details report-page">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Campos</th>
+                        <th scope="col">Datos ingresados</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Cantidad de horas necesarias para limpiar cada equipo informático</th>
+                        <td scope="col">{{userData.chFormatear}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Costo para limpiar cada equipo informático</th>
+                        <td scope="col">{{userData.cFormatear}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Cantidad de equipos informáticos que se pueden limpiar en paralelo</th>
+                        <td scope="col">{{userData.cantEquiposParaleloFormatear}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Cantidad de horas necesarias para restaurar la información en cada equipo informático</th>
+                        <td scope="col">{{userData.chRestaurar}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Costo para restaurar la información en cada equipo informático</th>
+                        <td scope="col">{{userData.cRestaurar}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Cantidad de equipos informáticos que se pueden restaurar en paralelo</th>
+                        <td scope="col">{{userData.cantEquiposParaleloRestaurar}}</td>
+                    </tr>
+                </tbody>
+                </table>
+
+                <table class="table table-striped table-bordered table-hover specific-scenario-details report-page">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Campos</th>
+                        <th scope="col">Datos ingresados</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Costo del rescate solicitado por el ciberdelincuente</th>
+                        <td scope="col">{{userData.cRescate}}</td>
+                    </tr>
+                </tbody>
+                </table>
+
+                <table class="table table-striped table-bordered table-hover specific-scenario-details report-page">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">Campos</th>
+                        <th scope="col">Datos ingresados</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">Porcentaje de equipos informáticos que deben recuperarse para que el incidente se considere controlado</th>
+                        <td scope="col">{{userData.porcenEquiposInfectados}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Costo de oportunidad diario producto de tener el negocio parado</th>
+                        <td scope="col">{{userData.cOportunidadVentas}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">¿Cuál piensa que sería el costo de reputación en el cual incurriría la organización debido a los problemas en su imagen?</th>
+                        <td scope="col">{{userData.cReputacion}}</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">Si los ciberdelincuentes filtran la información secuestrada (práctica conocida como Doxxing) ¿Cuál piensa que sería el costo que esta acción podría implicar para la organización?</th>
+                        <td scope="col">{{userData.cFiltradoInfo}}</td>
+                    </tr>
+                </tbody>
+                </table>
+                </div>
+            </div>          
 
         <div class="row">
             <div id="descripcion-escenarios" class="col-lg-12">
